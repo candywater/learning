04ca943b20a23bea6d2bb53c807fcc3b67bd3735
candywater <candywater@users.noreply.github.com>
2018-01-29 22:25:46 +0900

    fix error

--- video-shadow2avi.sh
+++ video-shadow2avi.sh
@@ -1,3 +1,6 @@
-for i in *; do
-	ffmpeg -i $i -r 30 -vsync cfr -af aresample=async=1 -acodec pcm_s16le -vcodec utvideo $i.mp4
+for i in *.mp4; do
+	ffmpeg -i "${i}" -r 30 -vsync cfr -af aresample=async=1 -acodec pcm_s16le -vcodec utvideo "${i}.avi"
+	echo ffmpeg -i "$i" -r 30 -vsync cfr -af aresample=async=1 -acodec pcm_s16le -vcodec utvideo $i.mp4
 done
+
+#use zsh
